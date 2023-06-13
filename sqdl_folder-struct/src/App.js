ec702a2aed7a372d583edb08b96c06083486ae4d
Prakhar Gupta <visitprakhar@gmail.com>
2023-06-14 00:10:49 +0530

    added file structure

---
+++ sqdl_folder-struct/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+// import NavBar from './components/homepage/NavBar.jsx'
+// import Footer from './components/homepage/Footer.js'
+// import SQDLCarousel from './components/homepage/Carousel.js'
+import Login from './components/lr/Pane.js'
+
+function App() {
+  return (
+      // <>
+      //   <NavBar loginHandler ='' registerHandler ='' aboutHandler =''/>
+      //   <SQDLCarousel/>
+      //   <Footer/>
+      // </>
+      <>
+
+      //can import Login, Register, or homepage from components here
+      <Login/>
+      </>
+    );
+}
+
+
+export default App;
