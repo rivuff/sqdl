f2c7dc8b9c00368157d6dce80fe5fa2dcc84be7e
Prakhar Gupta <visitprakhar@gmail.com>
2023-06-13 21:28:54 +0530

    frontend react for landing page, login, and sign up with empty handlers

---
+++ frontonly/homepage/Carousel.js
@@ -0,0 +1,42 @@
+import { Carousel, Typography } from "@material-tailwind/react";
+import { Button } from "@material-tailwind/react";
+
+export default function SQDLCarousel() {
+    return (
+        <Carousel className="rounded-xl">
+            <div className="relative h-full w-full">
+                <img
+                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+                    alt="image 1"
+                    className="h-full w-full object-cover"
+                />
+                <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/75">
+                    <div className="w-3/4 text-center md:w-2/4">
+                        <Typography
+                            variant="h1"
+                            color="white"
+                            className="mb-4 text-3xl md:text-4xl lg:text-5xl font-semibold"
+                        >
+                            Student Query Driven Learning
+                        </Typography>
+                        <Typography
+                            variant="lead"
+                            color="white"
+                            className="mb-12 opacity-80 text-white"
+                        >
+                            This SQDL App has been designed to enable some degree of self-directed learning in classroom environments
+                        </Typography>
+                        <div className="flex justify-center gap-6">
+                            <Button  color="white" variant ='text'>
+                                Login
+                            </Button>
+                            <Button  color="white" variant="text">
+                                Register
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Carousel>
+    );
+}
---
+++ frontonly/lr/Register.js
@@ -0,0 +1,237 @@
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+} from "@material-tailwind/react";
+import React from "react";
+// import {
+//     Tabs,
+//     TabsHeader,
+//     TabsBody,
+//     Tab,
+//     TabPanel,
+// } from "@material-tailwind/react";
+import {
+    Square3Stack3DIcon,
+    UserCircleIcon,
+    Cog6ToothIcon,
+} from "@heroicons/react/24/solid";
+
+
+function submitRegister(type){
+    if (type == 'student'){
+        var name = document.getElementById('studentName').value
+        var enrollment = document.getElementById('studentEnrollment').value
+        var password = document.getElementById('studentPassword').value
+        var post_val ={
+            'type': type,
+            'name': name,
+            'enrollment': enrollment,
+            'password': password,
+        }
+    }
+    else if (type =='teacher'){
+        var email = document.getElementById('teacherEmail').value
+        var password = document.getElementById('teacherPassword').value
+        var post_val={
+            'type':type,
+            'email':email,
+            'password':password
+        }
+    }
+    //submit post_val using axios
+    //route to next page with return, disable register button
+    console.log(post_val)
+    //when promise awaiting, disable submit buttons
+    //will do once backend is implemented
+    return null
+
+}
+//component design
+
+const Tabs = ({ color }) => {
+    const [openTab, setOpenTab] = React.useState(1);
+    return (
+        <>
+            <div className="flex flex-wrap">
+                <div className="w-full">
+                    <ul
+                        className="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row"
+                        role="tablist"
+                    >
+                        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                            <a
+                                className={
+                                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                                    (openTab === 1
+                                        ? "text-blue-400 bg--600"
+                                        : "text-" + color + "-600 bg-white")
+                                }
+                                onClick={e => {
+                                    e.preventDefault();
+                                    setOpenTab(1);
+                                }}
+                                data-toggle="tab"
+                                href="#link1"
+                                role="tablist"
+                            >
+                                Student
+                            </a>
+                        </li>
+                        <li className="-mb-px mr-2 last:mr-0 flex-auto text-center">
+                            <a
+                                className={
+                                    "text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal " +
+                                    (openTab === 2
+                                        ? "text-blue-400 bg-" + color + "-600"
+                                        : "text-blue" + "-600 bg-white")
+                                }
+                                onClick={e => {
+                                    e.preventDefault();
+                                    setOpenTab(2);
+                                }}
+                                data-toggle="tab"
+                                href="#link2"
+                                role="tablist"
+                            >
+                                Teacher
+                            </a>
+                        </li>
+                        
+                    </ul>
+                    <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+                        <div className="px-4 py-5 flex-auto">
+                            <div className="tab-content tab-space">
+                                <div className={openTab === 1 ? "block" : "hidden"} id="link1">
+                                <Form input ='student'/>
+                                </div>
+                                <div className={openTab === 2 ? "block" : "hidden"} id="link2">
+                                    <Form input='teacher' />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+function Form(props) {
+    if (props.input == 'student')
+ {   return (
+        <Card color="transparent" shadow={false}>
+            <Typography variant="h4" color="blue-gray">
+                Student Sign Up
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal">
+                Enter your details to register.
+            </Typography>
+            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" id ='studentRegister'>
+                <div className="mb-4 flex flex-col gap-6">
+                    <Input size="lg" id='studentName'label="Full Name" />
+                    <Input size="lg" id ='studentEnrollment'label="Enrollment Number" />
+                    <Input type="password" id='studentPassword' size="lg" label="Password" />
+                </div>
+                <Checkbox
+                    label={
+                        (
+                            <Typography
+                                variant="small"
+                                color="gray"
+                                className="flex items-center font-normal"
+                            >
+                                I agree to the
+                                <a
+                                    href="#"
+                                    className="font-medium transition-colors hover:text-blue-500"
+                                >
+                                    &nbsp;Terms and Conditions
+                                </a>
+                            </Typography>
+                        )
+                    }
+                    containerProps={{ className: "-ml-2.5" }}
+                />
+                <Button className="mt-6" fullWidth onClick={() => { submitRegister('student') }}>
+                    Register
+                </Button>
+                <Typography color="gray" className="mt-4 text-center font-normal">
+                    Already have an account?{" "}
+                    <a
+                        href="#"
+                        className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+                    >
+                        Sign In
+                    </a>
+                </Typography>
+            </form>
+        </Card>
+    );}
+    if (props.input =='teacher'){
+        return(
+            <Card color="transparent" shadow={false}>
+                <Typography variant="h4" color="blue-gray">
+                    Teacher Sign Up
+                </Typography>
+                <Typography color="gray" className="mt-1 font-normal">
+                    Enter your details to register.
+                </Typography>
+                <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" id ='teacherRegister'>
+                    <div className="mb-4 flex flex-col gap-6">
+                        <Input size="lg" id ='teacherEmail' label="Email" />
+                        <Input type="password" id ='teacherPassword' size="lg" label="Password" />
+                    </div>
+                    <Checkbox
+                        label={
+                            (
+                                <Typography
+                                    variant="small"
+                                    color="gray"
+                                    className="flex items-center font-normal"
+                                >
+                                    I agree to the
+                                    <a
+                                        href="#"
+                                        className="font-medium transition-colors hover:text-blue-500"
+                                    >
+                                        &nbsp;Terms and Conditions
+                                    </a>
+                                </Typography>
+                            )
+                        }
+                        containerProps={{ className: "-ml-2.5" }}
+                    />
+                    <Button className="mt-6" fullWidth onClick={() => { submitRegister('teacher') }}>
+                        Register
+                    </Button>
+                    <Typography color="gray" className="mt-4 text-center font-normal">
+                        Already have an account?{" "}
+                        <a
+                            href="#"
+                            className="font-medium text-blue-500 transition-colors hover:text-blue-700"
+                        >
+                            Sign In
+                        </a>
+                    </Typography>
+                </form>
+            </Card>
+        )
+    }
+}
+
+export default function Register() {
+    return (
+
+        <div className="align-center p-10 flex items-center justify-center h-screen ">
+            <div className="border-blue-400 border-4 rounded-lg p-10">
+
+
+                <Tabs color='blue'></Tabs>
+            </div>
+        </div>
+    );
+}
